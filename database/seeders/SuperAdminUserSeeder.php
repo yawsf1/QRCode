cffc9b3906f59ad086afcfba412de1f79364fb7d
yawsf1 <yousseflaayadiasape2@gmail.com>
2026-05-20 23:33:27 +0100

    fix: idempotent super admin seeder + run on deploy

--- database/seeders/SuperAdminUserSeeder.php
+++ database/seeders/SuperAdminUserSeeder.php
@@ -5,18 +5,20 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 class SuperAdminUserSeeder extends Seeder
 {
-    public function run(): void
-    {
-        User::firstOrCreate([
+public function run(): void
+{
+    User::firstOrCreate(
+        ['email' => 'k8e6Y@example.com'],
+        [
             'nom' => 'Admin',
             'prenom' => 'Admin',
-            'email' => 'k8e6Y@example.com',
             'password' => bcrypt('admin123'),
             'departement' => 'Admin',
             'telephone' => '1234567890',
             'est_actif' => true,
             'role' => 'super_admin',
             'admin_id' => null,
-        ]);
-    }
+        ]
+    );
+}
 }
